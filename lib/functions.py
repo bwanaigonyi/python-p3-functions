09ddc26c222a84b766aff05bbefbf663b3a4ed1d
barkley-wq <igonyibarkley00@gmail.com>
2024-09-16 11:13:16 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    print("Hello,programmer!")
+    print("Hello, programmer!")
 
 def greet(name):
-    print("Hello,{name}!")
-    
+    print(f"Hello, {name}!")
+
 def greet_with_default(name="programmer"):
-    print("Hello,{name}!")
-    
+    print(f"Hello, {name}!")
+
 def add(num1, num2):
     return num1 + num2
 
